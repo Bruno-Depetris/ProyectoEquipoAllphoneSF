ec6188a9b3ab4b6cab218671db08ddba276efef2
Bruno-Depetris <Depetrisbruno@gmail.com>
2024-10-04 00:00:05 +0100

    cascawdew

--- ProyectoAllphoneSF/Form1.cs
+++ ProyectoAllphoneSF/Form1.cs
@@ -12,6 +12,7 @@ namespace ProyectoAllphoneSF {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
         }
         private void LimpiarCampos() {
 
